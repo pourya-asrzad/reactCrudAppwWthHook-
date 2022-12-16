933973727aee82ac7fcc7594e8b3ee549bea2c04
Pouria Asrzad <poriaasrzad@gmail.com>
2022-12-16 16:18:08 +0330

    validation id finished

--- reactcrud/src/components/input/Input.js
+++ reactcrud/src/components/input/Input.js
@@ -3,7 +3,13 @@ import React, { useEffect, useState } from "react";
 const Input = (props) => {
   const { name, value, ongetvalue } = props;
   const [inputValue, setInputValue] = useState("");
+  const [invalid, setInvalid] = useState(true);
   function inputChangeHandler(e) {
+    setInvalid(true);
+    if (value && inputValue.trim().length < 5) {
+      setInvalid(false);
+      return;
+    }
     ongetvalue(e.target.value);
     setInputValue(e.target.value);
   }
@@ -16,12 +22,15 @@ const Input = (props) => {
       <label htmlFor={name}>{props.children} </label>
       <br />
       <input
+        className={!invalid ? "invalidinput" : ""}
         onChange={inputChangeHandler}
         value={inputValue}
         name={name}
         placeholder={name}
         id={name}
       />
+
+      {!invalid ? <span className="errormssege">invalid {name}</span> : ""}
     </div>
   );
 };
